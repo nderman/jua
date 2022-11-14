8587d8ef9e5e8771d65bbbc1c9e8afa8f1e7eb81
Neal Derman <nderman@gmail.com>
2022-11-14 20:41:28 +0200

    feat: add map view

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import './App.css'
+import Map from './components/map'
 
 const App: React.FunctionComponent = () => {
   return (
-    <div className="Jua">
-    </div>
+      <Map/>
   )
 }
 
